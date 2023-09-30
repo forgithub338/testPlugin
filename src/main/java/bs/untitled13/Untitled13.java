d0a600a1c3f98e4d8e96870c69150b3e7235fa4f
forgithub338 <125258480+forgithub338@users.noreply.github.com>
2023-09-30 17:13:27 +0800

    Update Untitled13.java

--- src/main/java/bs/untitled13/Untitled13.java
+++ src/main/java/bs/untitled13/Untitled13.java
@@ -7,6 +7,7 @@ public final class Untitled13 extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new Event(this), this);
+        getServer().getPluginManager().registerEvents(new Listener(), this);
 
     }
 
